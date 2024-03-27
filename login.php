0e6bdff5ee15cf9d0416dffdc7b30cf5f82ff5b1
David Bieger <dkb6ah@virginia.edu>
2024-03-27 12:00:03 -0400

    added style sheet and made login look more like template

--- login.php
+++ login.php
@@ -5,25 +5,27 @@ require("connect-db.php");
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+  <meta charset="utf-8">   
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <title>PHP: Form handling</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="styles.css" /> 
 </head>
-<body>
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" name="login" value="Login">
+<body>  
+  <div>  
+    <h1>PHP: Form Handling</h1>
+    <form action="login.php" method="post">     
+      Username: <input type="text" name="name" required /> <br/>
+      Password: <input type="password" name="pwd" required /> <br/>
+      <input type="submit" value="Submit" class="btn" />
     </form>
-    <br>
-    <form action="create_account.php" method="get">
-        <input type="submit" value="Create Account">
-    </form>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
 </body>
 </html>
 
