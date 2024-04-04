78f8eaf6fbf2e2f918b24cbe392e0f8036eb86ce
David Bieger <dkb6ah@virginia.edu>
2024-04-04 15:00:56 -0400

    create account button functions properly

--- login.php
+++ login.php
@@ -1,42 +1,8 @@
 <?php
 require("connect-db.php");
 require("database-functions.php");
-?>
 
-
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">   
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <title>Gym Tracker</title> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
-  <link rel="stylesheet" href="styles.css" /> 
-</head>
-<body>  
-  <div>  
-    <h1>Gym Tracker</h1>
-    <form action="login.php" method="post">     
-      Username: <input type="text" name="username" required /> <br/>
-      Password: <input type="password" name="password" required /> <br/>
-      <input type="submit" name="Submit" value="Submit" class="btn" />
-      <input type="submit" name="create-account" value="Create Account" class="btn" /> 
-    </form>
-  </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-</body>
-</html>
-
-
-
-<?php
-
-
-//STUB: should be able to have validate user function only in the database-functions file, but that is currently not working.
+// STUB: should be able to have validate user function only in the database-functions file, but that is currently not working.
 function validateUser($username, $password) {
     global $db;
     $query = 'SELECT password FROM Users WHERE user_id = :user_id';
@@ -70,10 +36,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Invalid username or password. Please try again.";
         }
     }
-    if (!empty($_POST['create-account'])) {
-      //redirect user to create-account.php
-      header("Location: http://localhost/cs4750/DatabaseSystemsFinal/create-account.php"); //use this to redirect to new files
-      exit;
-  }
+}
+
+// Check if "Create Account" form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['create-account-submit'])) {
+    // Redirect user to create-account.php
+    header("Location: http://localhost/cs4750/DatabaseSystemsFinal/create-account.php");
+    exit;
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">   
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <title>Gym Tracker</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="styles.css" /> 
+</head>
+<body>  
+  <div>  
+    <h1>Gym Tracker</h1>
+    <form id="loginForm" action="login.php" method="post">     
+      Username: <input type="text" name="username" required /> <br/>
+      Password: <input type="password" name="password" required /> <br/>
+      <input type="submit" name="Submit" value="Submit" class="btn" />
+    </form>
+    
+    <!-- Create Account form -->
+    <form id="createAccountForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <input type="hidden" name="create-account-submit" value="true">
+        <input type="submit" value="Create Account" class="btn" />
+    </form>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
