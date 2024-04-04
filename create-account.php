c7e44e7e80fd357dbbb880fd1ae2085b789f926d
David Bieger <dkb6ah@virginia.edu>
2024-04-04 15:07:01 -0400

    started create account stuff

--- create-account.php
+++ create-account.php
@@ -1,22 +1,41 @@
 <?php
 //haven't done much with this yet at all
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST['Submit'])) {
+        $username = $_POST["username"];
+        $password1 = $_POST["password"];
+        $password2 = $_POST["confirm password"];
+        if ($password1 != $password2)  {
+            echo "Passwords must match, please try again.";
+        } else if (strlen($password1) < 10) {
+            echo "Password must be at least 10 characters long. Try again."; // used for cyber 
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Account</title>
+  <meta charset="utf-8">   
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  <!-- required to handle IE -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <title>Create Account</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="styles.css" /> 
 </head>
-<body>
-    <h2>Create Account</h2>
-    <form action="process_account.php" method="post">
-        <label for="new_username">New Username:</label><br>
-        <input type="text" id="new_username" name="new_username"><br>
-        <label for="new_password">New Password:</label><br>
-        <input type="password" id="new_password" name="new_password"><br><br>
-        <input type="submit" value="Create Account">
+<body>  
+  <div>  
+    <h1>Gym Tracker</h1>
+    <form id="createAccountForm" action="create-account.php" method="post">     
+      Username: <input type="text" name="username" required /> <br/>
+      Password: <input type="password" name="password" required /> <br/>
+      Confirm Password: <input type="password" name="confirm password" required /> <br/>
+      <input type="submit" name="Submit" value="Create Account" class="btn" />
     </form>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
 </body>
 </html>
