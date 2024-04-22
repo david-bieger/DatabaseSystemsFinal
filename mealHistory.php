b0e0f652bf82f40e2e54878da0e930f3c111898a
KieranOdell <kfo7qb@virginia.edu>
2024-04-22 12:58:59 -0400

    Meal history works mostly

--- mealHistory.php
+++ mealHistory.php
@@ -1,4 +1,86 @@
 <?php
-echo "mealhistory";
-$username = $_GET['username'];
+require("connect-db.php");
+require("database-functions.php");
+
+$username = "David";
+
+// Check if a delete request is made
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_id'])) {
+    $deleteId = $_POST['delete_id'];
+    $meal_number = $_POST['meal_number'];
+    $date = $_POST['date'];
+    
+    $query = "DELETE FROM Meal_History WHERE user_id = :user_id AND date = :date AND meal_number = :meal_number";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':user_id', $username);
+    $statement->bindValue(':date', $date);
+    $statement->bindValue(':meal_number', $meal_number);
+    $statement->execute();
+}
+
+$query = "SELECT * FROM Meal_History WHERE user_id = :user_id ORDER BY date DESC";
+$statement = $db->prepare($query);
+$statement->bindValue(':user_id', $username);
+$statement->execute();
+$meals = $statement->fetchAll(PDO::FETCH_ASSOC);
+$statement->closeCursor();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">   
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+  <meta name="viewport" content="width=device-width, initial-scale=1">  
+  <title>Meal History</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="styles.css" /> 
+</head>
+<body>  
+  <div>  
+    <h1>Meal History</h1>
+    <?php if (count($meals) > 0): ?>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Calories</th>
+          <th>Carbs</th>
+          <th>Protein</th>
+          <th>Fat</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($meals as $meal): ?>
+        <tr>
+          <td><?php echo $meal['date']; ?></td>
+          <td><?php echo $meal['calories']; ?></td>
+          <td><?php echo $meal['carbs']; ?></td>
+          <td><?php echo $meal['protein']; ?></td>
+          <td><?php echo $meal['fat']; ?></td>
+          <td>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <input type="hidden" name="delete_id" value="<?php echo $meal['user_id']; ?>">
+                <input type="hidden" name="meal_number" value="<?php echo $meal["meal_number"]; ?>">
+                <input type="hidden" name="date" value="<?php echo $meal['date']; ?>">
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+    <?php else: ?>
+    <p>No meal history found for this user.</p>
+    <?php endif; ?>
+    <form id="home" action="home.php" method="get">
+        <input type="hidden" name="username" value="<?php echo $username; ?>">
+        <input type="submit" value="Home" class="btn" />
+    </form>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
