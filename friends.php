cea483b9c57391a3d177a4fe241edf666dc5c3e6
Wyndham <wrw2ztk@virginia.edu>
2024-04-23 10:20:37 -0400

    made friend requests display properly. Made the form for submitting a friend request but it doesn't work yet.

--- friends.php
+++ friends.php
@@ -19,15 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 
-// //Check if a delete request is made
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $user_id2 = $_POST['user_id2'];
-//     $query = "DELETE FROM friends WHERE user_id1 = $username AND user_id2 = :user_id2";
-//     $statement = $db->prepare($query);
-//     $statement->bindValue(':set_number', $set_number);
-//     $statement->execute();
-// }
-
 $query = "SELECT * FROM (friends JOIN Users ON friends.user_id2 = Users.user_id) WHERE user_id1 = :user_id1";
 
 $statement = $db->prepare($query);
@@ -36,7 +27,42 @@ $statement->execute();
 $friends = $statement->fetchAll(PDO::FETCH_ASSOC);
 $statement->closeCursor();
 
+$query2 = "SELECT * FROM (friends JOIN Users ON friends.user_id1 = Users.user_id) WHERE user_id2 = :user_id2";
+
+$statement2 = $db->prepare($query2);
+$statement2->bindValue(':user_id2', $username);
+$statement2->execute();
+$friends2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+$statement2->closeCursor();
+
+$query_requests = "SELECT * FROM friend_requests WHERE user_id2 = :user_id2";
+
+$statement3 = $db->prepare($query_requests);
+$statement3->bindValue(':user_id2', $username);
+$statement3->execute();
+$requests = $statement3->fetchAll(PDO::FETCH_ASSOC);
+$statement3->closeCursor();
+
+function addRequest($user_id1, $user_id2) {
+    global $db;
+    $query = 'INSERT INTO friend_requests (user_id1, user_id2) 
+    VALUES 
+    (:user_id1, :user_id2)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':user_id1', $username);
+    $statement->bindValue(':user_id2', $user_id2);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if (!empty($_POST['Submit'])) {
+        $username = $_POST['username'];
+        $user_id2 = $_POST["request_id"];
+        addRequest($username, $user_id2);
+    }
+
     if (!empty($_POST['Home'])) {
         $username = $_POST['username'];
         header("Location: http://localhost/cs4750/DatabaseSystemsFinal/home.php?username=$username");
@@ -58,7 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>  
   <div>  
     <h1>Friends</h1>
-    <?php if (count($friends) > 0): ?>
+    <?php if ((count($friends) + count($friends2)) > 0): ?>
     <table class="table">
       <thead>
         <tr>
@@ -79,16 +105,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           </td>
         </tr>
         <?php endforeach; ?>
+        <?php foreach ($friends2 as $friend2): ?>
+        <tr>
+          <td><?php echo $friend2['user_id1']; ?></td>
+          <td><?php echo $friend2['squat_max']; ?></td>
+          <td><?php echo $friend2['bench_max']; ?></td>
+          <td><?php echo $friend2['dl_max']; ?></td>
+          <td>
+          </td>
+        </tr>
+        <?php endforeach; ?>
       </tbody>
     </table>
     <?php else: ?>
     <p>No friends found for this user.</p>
     <?php endif; ?>
+    <?php if (count($requests) > 0): ?>
+    <h1>Incoming Friend Requests</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($requests as $request): ?>
+        <tr>
+          <td><?php echo $request['user_id1']; ?></td>
+          <td>
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+    <?php else: ?>
+    <p>No requests found for this user.</p>
+    <?php endif; ?>
+    <h1>Send Friend Request</h1>
+    <form id="sendRequestForm" action="friends.php" method="post">
+      Friend's Username: <input type="text" name="request_id" required /> <br/>
+      <input type="submit" name="Submit" value="Submit" class="btn" />
+    </form>
     <form id="home" action="http://localhost/cs4750/DatabaseSystemsFinal/home.php" method="get">
         <!-- Pass the username as a query parameter -->
         <input type="hidden" name="username" value="<?php echo $username; ?>" /> 
         <input type="submit" value="Home" class="btn" />
     </form>
+    
     </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
